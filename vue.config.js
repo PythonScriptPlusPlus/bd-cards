a586b4b6965f3d7ad76a6af65e2c53e4dbea7250
George Stones <george@MacBook-Air-George-2.local>
2024-11-12 15:07:02 +0300

    setup

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
-const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-});
+module.exports = {
+  chainWebpack: (config) => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    // eslint-disable-next-line
+		types.forEach((type) => addStyleResource(config.module.rule('scss').oneOf(type)));
+  },
+};
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/*.scss'),
+        path.resolve(__dirname, './src/assets/styles/*.css'),
+      ],
+    });
+}
